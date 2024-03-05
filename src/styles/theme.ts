3e3c1d115ccc43ea262c1c315544b75a3936f5e3
lysialeao <lysialeao@gmail.com>
2024-03-05 10:55:14 -0300

    style: :lipstick: initial config

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const Theme = {
+    color: {
+      black: "#281f27",
+      gray: "#665b65",
+      white: "#fcfbfc",
+    },
+    padding: {
+      small: "12px",
+      medium: "24px",
+      large: "48px",
+      xlarge: "96px"
+    },
+    gap: {
+      small: '4px'
+    }
+}
